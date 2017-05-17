39b01f7567bd5cb0860578b7b51714a23e9e5ac7
Kristof Majoros <akmajoros1@gmail.com>
2017-05-17 10:56:29 +0200

    theoretically I should have a working index page

---
+++ chatapp/src/main/java/com/greenfox/chatapp/controller/MainRestController.java
@@ -0,0 +1,9 @@
+package com.greenfox.chatapp.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MainRestController {
+
+}
