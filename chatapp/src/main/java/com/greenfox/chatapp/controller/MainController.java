796e4f4aadc70166f407644fd559921559b77dbd
Kristof Majoros <akmajoros1@gmail.com>
2017-05-17 13:49:46 +0200

    created environmental variable

--- chatapp/src/main/java/com/greenfox/chatapp/controller/MainController.java
+++ chatapp/src/main/java/com/greenfox/chatapp/controller/MainController.java
@@ -9,8 +9,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class MainController {
 
   @RequestMapping(value = "/")
-  public String indexPage(){
-    System.out.println(new Log("INFO", "/", "GET", "stuff"));
+  public String indexPage() {
+    String currentLogLevel = System.getenv("CHAT_APP_LOGLEVEL");
+    if (currentLogLevel != null && currentLogLevel.equals("INFO")) {
+      System.out.println(new Log("INFO", "/", "GET", "stuff"));
+    } else {
+      System.out.println("No INFO in loglevel variable");
+    }
     return "index";
   }
 }
