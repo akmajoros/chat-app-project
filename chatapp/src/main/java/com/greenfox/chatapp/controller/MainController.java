084c348ef928aa9cee926c243609449e3685a2af
Kristof Majoros <akmajoros1@gmail.com>
2017-05-17 15:30:47 +0200

    created /enter endpoint

--- chatapp/src/main/java/com/greenfox/chatapp/controller/MainController.java
+++ chatapp/src/main/java/com/greenfox/chatapp/controller/MainController.java
@@ -3,19 +3,39 @@ package com.greenfox.chatapp.controller;
 import com.greenfox.chatapp.model.Log;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.NoHandlerFoundException;
 
 @Controller
 public class MainController {
+
+  String chatAppUniqueId;
+  String chatAppPeerAddress;
+
+  public MainController() {
+    this.chatAppUniqueId = System.getenv("CHAT_APP_UNIQUE_ID");
+    this.chatAppPeerAddress = System.getenv("CHAT_APP_PEER_ADDRESS");
+  }
+
+  @ExceptionHandler(value = NoHandlerFoundException.class)
+  public String notFound() {
+    System.out.println("ERROR");
+    return "index";
+  }
 
   @RequestMapping(value = "/")
   public String indexPage() {
     String currentLogLevel = System.getenv("CHAT_APP_LOGLEVEL");
     if (currentLogLevel != null && currentLogLevel.equals("INFO")) {
       System.out.println(new Log("INFO", "/", "GET", "stuff"));
-    } else {
-      System.out.println("No INFO in loglevel variable");
     }
     return "index";
+  }
+
+  @RequestMapping(value = "/enter")
+  public String registerPage() {
+    return "enter";
   }
 }
